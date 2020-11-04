0be8dadc0a06642495858f027b5eb8b5d647b122
petar-stoyanov <pesho.stoyanov@gmail.com>
2020-11-04 17:24:32 +0200

    add few steps to functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -10,9 +10,14 @@ class UserVisitTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_opens_home_page(self):
-        self.browser.get('http://localhost:3000')
+    def test_opens_quiz_page(self):
+        # Joe har received a link to a quiz sent over from a friend
+        self.browser.get('http://localhost:3000/abcd')
         self.assertIn('Quiz', self.browser.title)
+
+        # Joe sees a question and reads it
+        # quiestion_text = self.browser.find_element_by_tag_name('.question-content').text
+        # self.assertIn('To-Do', quiestion_text)
 
 
 if __name__ == '__main__':
