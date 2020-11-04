aed92918d4c4584bbdbc3dfc44249b03cae9b923
petar-stoyanov <pesho.stoyanov@gmail.com>
2020-11-04 17:33:04 +0200

    add test_guid_link_resolves_to_quiz_page_view unit test

--- quizes/tests.py
+++ quizes/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import resolve
+from .views import quiz_page
+
+
+class QuizPageTest(TestCase):
+
+    def test_guid_link_resolves_to_quiz_page_view(self):
+        found = resolve('/abcd')
+        self.assertEqual(found.func, quiz_page)
